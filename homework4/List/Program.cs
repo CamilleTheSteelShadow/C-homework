509b078403fd4e8c0f343d43022512a4a02f606a
CamilleTheSteelShadow <79971465+CamilleTheSteelShadow@users.noreply.github.com>
2021-06-19 21:33:16 +0800

    Update Program.cs

--- homework4/List/Program.cs
+++ homework4/List/Program.cs
@@ -1,82 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ch02_3.GenericList;
 
-namespace List
+namespace ch02_3
 {
-    public class Node<T>
-    {
-        public Node<T> Next { get; set; }
-        public T Data { get; set; }
-
-        public Node(T t)
-        {
-            Next = null;
-            Data = t;
-        }
-    }
-
-    //泛型链表类
-    public class GenericList<T>
-    {
-        private Node<T> head;
-        private Node<T> tail;
-
-        public GenericList()
-        {
-            tail = head = null;
-        }
-
-        public Node<T> Head
-        {
-            get => head;
-        }
-
-        public void Add(T t)
-        {
-            Node<T> n = new Node<T>(t);
-            if (tail == null)
-            {
-                head = tail = n;
-            }
-            else
-            {
-                tail.Next = n;
-                tail = n;
-            }
-        }
-
-        public void ForEach(Action<T> action)
-        {
-            for (Node<T> n = head; n != null; n = n.Next)
-            {
-                action(n.Data);
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            GenericList<int> intlist = new GenericList<int>();
-            for (int x = 0; x < 10; x++)
-            {
-                intlist.Add(random.Next(1000));
-            }
+            GenericList<int> myList = new GenericList<int>();
+            myList.Add(10);
+            myList.Add(5);
+            myList.Add(-1);
 
-            intlist.ForEach(n => Console.Write(n + "\t"));
+            myList.ForEach(d => Console.WriteLine(d));
 
-            double min = double.MaxValue;
-            double max = double.MinValue;
-            double sum = 0;
-            intlist.ForEach(n => {
-                min = (n < min) ? n : min;
-                max = (n > max) ? n : max;
-                sum += n;
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+            long sum = 0;
+            myList.ForEach(d => {
+                if (d >= maxNum) maxNum = d;
+                if (d <= minNum) minNum = d;
+                sum += d;
             });
-            Console.WriteLine();
-            Console.WriteLine($"min={min},max={max},sum={sum}");
+            Console.WriteLine(string.Format("max: {0}\tmin: {1}\tsum: {2}", maxNum, minNum, sum));
         }
-
     }
 }
