17878fcca85a29bc23cb805b1bdd3ca9090c49ba
CamilleTheSteelShadow <79971465+CamilleTheSteelShadow@users.noreply.github.com>
2021-06-19 21:42:51 +0800

    Update OrderServiceTests.cs

--- homework6/OrderSerializeTests/OrderServiceTests.cs
+++ homework6/OrderSerializeTests/OrderServiceTests.cs
@@ -1,160 +1,360 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OrderApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ch03;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace OrderApp.Tests {
-  [TestClass]
-  public class OrderServiceTests {
+namespace ch03.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        private OrderService orderService = new OrderService();
 
-    OrderService orderService = new OrderService();
+        /*Commodities*/
+        private static Commodity basketball = new Commodity("Basketball", 62);     // 为了便于CRUD，把这些变量作为类成员
+        private static Commodity bible = new Commodity("Bible", 25);
+        private static Commodity tea = new Commodity("Tea", 28);
+        private static Commodity iPad = new Commodity("iPad", 1000);
 
-    Goods apple = new Goods(1,"apple", 10.0f);
-    Goods egg = new Goods(2, "egg", 1.2f);
-    Goods milk = new Goods(3, "milk", 50f);
-    Customer customer1 = new Customer(1,"Customer1");
-    Customer customer2 = new Customer(2, "Customer2");
+        /*Customers*/
+        private static Customer james = new Customer("James", 40);
+        private static Customer underwood = new Customer("Underwood", 61);
+        private static Customer tim = new Customer("Tim", 19);
 
-    [TestInitialize]
-    public void Init() {
+        /*OrderDetails*/
+        private static OrderDetail od1 = new OrderDetail(basketball, 5, 0.9F);
+        private static OrderDetail od2 = new OrderDetail(bible, 1);
+        private static OrderDetail od3 = new OrderDetail(tea, 6);
+        private static OrderDetail od4 = new OrderDetail(iPad, 1, 0.75F);
 
-      Order order1 = new Order(1, customer1,new DateTime(2021, 3, 21));
-      order1.AddDetails(new OrderDetail(apple, 80));
-      order1.AddDetails(new OrderDetail(egg, 200));
-      order1.AddDetails(new OrderDetail(milk, 10));
+        private Order order1 = new Order();
+        private Order order2 = new Order();
 
-      Order order2 = new Order(2, customer2, new DateTime(2021, 3, 21));
-      order2.AddDetails(new OrderDetail(egg, 200));
-      order2.AddDetails(new OrderDetail(milk, 10));
+        [TestInitialize]
+        public void Init()
+        {
+            order1.Id = 1;
+            order1.Customer = james;        // James对应Order1，有两条订单明细
+            order1.AddOrderDetail(od1);     
+            order1.AddOrderDetail(od2);
 
-      Order order3 = new Order(3, customer2, new DateTime(2021, 3, 21));
-      order3.AddDetails(new OrderDetail(apple, 80));
-      order3.AddDetails(new OrderDetail(milk, 10));
+            order2.Id = 2;
+            order2.Customer = underwood;    // Underwood对应Order2，有一条订单明细
+            order2.AddOrderDetail(od3);
 
-      orderService = new OrderService();
-      orderService.AddOrder(order1);
-      orderService.AddOrder(order2);
-      orderService.AddOrder(order3);
+            orderService.AddOrder(order2);  // 这里反过来放入是为了后面排序便于验证
+            orderService.AddOrder(order1);
+        }
+
+        [TestMethod()]
+        public void AddCorrectOrderTest()
+        {
+            Order order = new Order         // 订单号不与已有订单重复，是正确的订单
+            {
+                Id = 3,
+                Customer = tim
+            };
+            order.AddOrderDetail(od4);
+
+            orderService.AddOrder(order);
+
+            bool assertFlag = false;
+            var orders = orderService.SelectAll();
+            if (orders.Contains(order))
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void AddWrongOrderTest()
+        {
+            Order order = new Order         // 订单号与已有订单重复，是错误的订单
+            {
+                Id = 2,
+                Customer = tim
+            };
+            order.AddOrderDetail(od4);
+            orderService.AddOrder(order);
+        }
+
+        [TestMethod()]
+        public void DeleteExistingOrderTest()
+        {
+            bool assertFlag = false;
+            orderService.DeleteOrder(1);        // 删除order1
+
+            var orders = orderService.SelectAll();
+            if (!orders.Contains(order1))
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void DeleteInexistingOrderTest()
+        {
+            orderService.DeleteOrder(3);
+            Assert.AreEqual(orderService.SelectAll().Count(), 2);
+        }
+
+        [TestMethod()]
+        public void UpdateExistingOrderTest()
+        {
+            bool assertFlag = false;
+
+            james.Age = 41;
+            Order order = orderService.SearchWithId(1);      //将订单找出来之后进行修改
+            order.Customer = james;
+            orderService.UpdateOrder(order);
+
+            var orderNamedJames = orderService.SearchWithId(1);
+            if (orderNamedJames != null)
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void UpdateInexistingOrderTest()
+        {
+            Order order = new Order         // 订单号与已有订单重复，是错误的订单
+            {
+                Id = 3,
+                Customer = tim
+            };
+            order.AddOrderDetail(od4);
+
+            orderService.UpdateOrder(order);       // 修改一个不存在的order
+            Assert.AreEqual(orderService.SelectAll().Count(), 3);
+            Assert.IsNotNull(orderService.SearchWithId(3));
+        }
+
+        [TestMethod()]
+        public void SearchWithIdTest()
+        {
+            var order1 = orderService.SearchWithId(1);
+            var order2 = orderService.SearchWithId(2);
+
+            Assert.IsNotNull(order1);
+            Assert.IsNotNull(order2);
+        }
+
+        [TestMethod()]
+        public void SearchWithInexistingIdTest()
+        {
+            var order3 = orderService.SearchWithId(3);
+            Assert.IsNull(order3);
+        }
+
+        [TestMethod()]
+        public void SearchWithNameTest()
+        {
+            bool assertFlag = false;
+
+            var order1 = orderService.SearchWithName(basketball.Name);
+            var order2 = orderService.SearchWithName(tea.Name);
+
+            if (order1.Count() != 0 && order2.Count() != 0)
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SearchWithInexistingNameTest()
+        {
+            bool assertFlag = false;
+
+            var order3 = orderService.SearchWithName(iPad.Name);
+
+            if (order3.Count() == 0)
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SearchWithCustomerTest()
+        {
+            bool assertFlag = false;
+
+            var order1 = orderService.SearchWithCustomer(james);
+            var order2 = orderService.SearchWithCustomer(underwood);
+
+            if (order1.Count() != 0 && order2.Count() != 0)
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SearchWithInexistingCustomerTest()
+        {
+            bool assertFlag = false;
+
+            var order3 = orderService.SearchWithCustomer(tim);
+
+            if (order3.Count() == 0)
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SearchWithMoneyRangeTest()
+        {
+            bool assertFlag = false;
+            double lower = od1.Money + od2.Money - 10;
+            double upper = od1.Money + od2.Money + 10;
+
+            var orders = orderService.SearchWithMoney(lower, upper);
+            if (orders.Count() != 0)
+            {
+                assertFlag = true;
+            }
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SearchWithMoneyTest()
+        {
+            bool assertFlag = false;
+            double money = od1.Money + od2.Money;
+
+            var orders = orderService.SearchWithMoney(money);
+            if (orders.Count() == 1)
+            {
+                assertFlag = true;
+            }
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SortTest()
+        {
+            bool assertFlag = true;
+
+            Order order3 = new Order
+            {
+                Id = 3,
+                Customer = tim
+            };
+            order3.AddOrderDetail(od4);
+            orderService.AddOrder(order3);       // 此时order顺序为2、1、3
+
+            orderService.Sort();
+            var orders = orderService.SelectAll();
+            int lastId = orders.First().Id;
+            foreach (var order in orders)
+            {
+                if (order.Id < lastId)
+                {
+                    assertFlag = false;
+                    break;
+                }
+                lastId = order.Id;
+            }
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void SortLambdaTest()
+        {
+            bool assertFlag = true;
+
+            Order order3 = new Order
+            {
+                Id = 3,
+                Customer = tim
+            };
+            order3.AddOrderDetail(od4);
+            orderService.AddOrder(order3);
+
+            orderService.Sort((o1, o2) => { return Convert.ToInt32(o1.Money - o2.Money); });    //按金额排序
+            var orders = orderService.SelectAll();
+            double lastMoney = orders.First().Money;
+            foreach (var order in orders)
+            {
+                if (order.Money < lastMoney)
+                {
+                    assertFlag = false;
+                    break;
+                }
+                lastMoney = order.Money;
+            }
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        public void ExportToCorrectPathTest()
+        {
+            bool assertFlag = false;
+
+            try
+            {
+                orderService.Export("./orders.xml");
+                assertFlag = true;
+                File.Delete("./orders.xml");    // 用完删除，不影响下一次测试
+            }
+            catch (Exception) { }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExportToWrongPathTest()
+        {
+            orderService.Export("./orders.txt");
+        }
+
+        [TestMethod()]
+        public void ImportFromCorrectPathTest()
+        {
+            bool assertFlag = false;
+
+            orderService.Export("./orders.xml");    // 先导出再导入
+
+            OrderService os = new OrderService();
+            os.Import("./orders.xml");
+
+            File.Delete("./orders.xml");
+
+            var orders = os.SelectAll();
+            if (orders.Count() != 0)
+            {
+                assertFlag = true;
+            }
+
+            Assert.IsTrue(assertFlag);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ImportFromWrongPathTest()
+        {
+            OrderService os = new OrderService();
+            os.Import("./non-orders.xml");
+        }
     }
-
-    [TestMethod]
-    public void AddOrderTest() {
-      Order order4 = new Order(4,customer2, new DateTime(2021, 3, 21));
-      order4.AddDetails(new OrderDetail(milk, 10));
-      orderService.AddOrder(order4);
-      List<Order> orders = orderService.QueryAll();
-      Assert.AreEqual(4, orders.Count);
-      CollectionAssert.Contains(orders, order4);
-    }
-
-
-    [TestMethod]
-    [ExpectedException(typeof(ApplicationException))]
-    public void AddOrderTest2() {
-      Order order4 = new Order(3, customer2, new DateTime(2021, 3, 21));
-      orderService.AddOrder(order4);
-    }
-
-
-    [TestMethod]
-    public void RemoveOrderTest() {
-      orderService.RemoveOrder(3);
-      List<Order> orders = orderService.QueryAll();
-      Assert.AreEqual(2, orders.Count);
-      orderService.RemoveOrder(100);
-      Assert.AreEqual(2, orders.Count);
-    }
-
-    [TestMethod]
-    public void UpdateOrderTest() {
-      Order order3 = new Order(3, customer1, new DateTime(2021, 3, 21));
-      order3.AddDetails(new OrderDetail(milk, 200));
-      orderService.Update(order3);
-
-      List<Order> orders = orderService.QueryAll();
-      Assert.AreEqual(3, orders.Count);
-      Order o = orderService.GetById(3);
-      Assert.AreEqual(customer1, o.Customer);
-    }
-
-
-    [TestMethod]
-    public void QueryOrderByIdTest() {
-      Order order2=orderService.GetById(2);
-      Assert.IsNotNull(order2);
-      Assert.AreEqual(2, order2.Id);
-      Assert.AreEqual(customer2, order2.Customer);
-      List<OrderDetail> details = new List<OrderDetail>() 
-        { new OrderDetail(egg, 200), new OrderDetail(milk, 10) };
-      CollectionAssert.AreEqual(details, order2.Details);
-
-      Order order4 = orderService.GetById(4);
-      Assert.IsNull(order4);
-    }
-
-    
-    [TestMethod]
-    public void QueryOrdersByGoodsNameTest() {
-      Assert.AreEqual(2,orderService.QueryByGoodsName("apple").Count);
-      Assert.AreEqual(2,orderService.QueryByGoodsName("egg").Count);
-      Assert.AreEqual(3,orderService.QueryByGoodsName("milk").Count);
-      Assert.AreEqual(0,orderService.QueryByGoodsName("orange").Count);
-    }
-
-    [TestMethod]
-    public void QueryOrdersByCustomerNameTest() {
-      Assert.AreEqual(1,orderService.QueryByCustomerName("Customer1").Count);
-      Assert.AreEqual(2,orderService.QueryByCustomerName("Customer2").Count);
-      Assert.AreEqual(0,orderService.QueryByCustomerName("Customer3").Count);
-    }
-
-    
-    [TestMethod]
-    public void ExportTest() {
-      String file = "temp.xml";
-      orderService.Export(file);
-      Assert.IsTrue(File.Exists(file));
-
-      String result = File.ReadAllText(file);
-      String expect=File.ReadAllText("../../expectedOrders.xml");
-      Assert.AreEqual(expect, result);
-
-      File.Delete(file); //Clear up
-    }
-
-    [TestMethod]
-    public void ImportTest1() {
-      //OrderService orderService = new OrderService();
-      List<Order> expect = orderService.QueryAll();
-      orderService.Import("../../expectedOrders.xml");
-      List<Order> result = orderService.QueryAll();
-      CollectionAssert.Equals(expect, result);
-
-      orderService.Import("../../newOrders.xml");
-      result = orderService.QueryAll();
-      Assert.AreEqual(4, result.Count);
-      Assert.IsTrue(result.Any(o => o.Id == 4));
-    }
-
-  [TestMethod]
-  [ExpectedException(typeof(FileNotFoundException))]
-  public void ImportTest3() {
-    OrderService os = new OrderService();
-    os.Import("../../orders.xml");
-  }
-
-  [TestMethod]
-  [ExpectedException(typeof(InvalidOperationException))]
-  public void ImportTest4() {
-    OrderService os = new OrderService();
-    os.Import("../../invalidXML.xml");
-  }
-  
-
-  }
 }
